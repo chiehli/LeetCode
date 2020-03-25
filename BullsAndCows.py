2ce586bb0219038f82e01bd81654a2e5a1347059
chiehli <chiehli.chen@gmail>
2020-03-24 21:45:56 -0700

    edited to use one hash table

--- BullsAndCows.py
+++ BullsAndCows.py
@@ -17,8 +17,7 @@ class BullsAndCows():
 
         bullCount = 0
         cowCount = 0
-        secretTable = {}
-        guessTable = {}
+        table = {}
         for i in range(len(secret)):
             gDigit = guess[i]
             sDigit = secret[i]
@@ -28,22 +27,25 @@ class BullsAndCows():
                 bullCount += 1
             else:
                 # Digits do not match
-                # Add the not matched digits to corresponding hash table
-                if sDigit in secretTable:
-                    secretTable[sDigit] += 1
+                # Add the not matched digits to table and update cowCount
+                # if is sDigit, table[sDigit] += 1,
+                # if is gDigit, table[gDigit] -= 1
+                # if table[sDigit] < 0 -> the digit appears in guess, 
+                # -> cowCount += 1
+                # if table[gDigit] > 0 -> the digit appears in secret,
+                # -> cowCount += 1
+                if sDigit in table:
+                    if table[sDigit] < 0:
+                        cowCount += 1
+                    table[sDigit] += 1
                 else:
-                    secretTable[sDigit] = 1
-                if gDigit in guessTable:
-                    guessTable[gDigit] += 1
+                    table[sDigit] = 1
+                if gDigit in table:
+                    if table[gDigit] > 0:
+                        cowCount += 1
+                    table[gDigit] -= 1
                 else:
-                    guessTable[gDigit] = 1
-
-        # Check the number of Cows (B)
-        for num in guessTable:
-            gCount = guessTable[num]
-            if num in secretTable:
-                sCount = secretTable[num]
-                cowCount += min(gCount, sCount)
+                    table[gDigit] = -1
 
         ans = '{0}A{1}B'.format(bullCount, cowCount)
         return ans
@@ -62,6 +64,11 @@ def main():
     print(bull_n_cow.getHint(secret, guess))
 
     guess = '0707'
+    print('guess: ' + guess)
+    print(bull_n_cow.getHint(secret, guess))
+
+    secret = '1123'
+    guess = '0111'
     print('guess: ' + guess)
     print(bull_n_cow.getHint(secret, guess))
 
