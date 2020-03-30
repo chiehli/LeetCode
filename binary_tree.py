e03bc822861f491bc0fec86ca5bf94cf455d482e
chiehli <chiehli.chen@gmail>
2020-03-30 14:04:33 -0700

    adding level order print and check if symmetric

--- binary_tree.py
+++ binary_tree.py
@@ -2,6 +2,7 @@
 A class for binary tree
 """
 import math
+from collections import deque
 
 class TreeNode(object):
     def __init__(self, x):
@@ -20,7 +21,7 @@ class TreeNode(object):
     def left(self):
         # rtype: TreeNode
         if not self._left:
-            return 'null'
+            return None
         return self._left
 
     @left.setter
@@ -31,12 +32,18 @@ class TreeNode(object):
     def right(self):
         # rtype: TreeNode
         if not self._right:
-            return 'null'
+            return None
         return self._right
 
     @right.setter
     def right(self, tnode):
         self._right = tnode
+
+    def is_leaf(self):
+        if not self._left and not self._right:
+            return True
+        else:
+            return False
 
     def print_node(self):
         val = 'null'
@@ -45,21 +52,30 @@ class TreeNode(object):
 
         if self:
             val = self._value
+        else:
+            val = 'null'
 
         if self._left:
             leftVal = self._left._value
+        else:
+            leftVal = 'null'
 
         if self._right:
             rightVal = self._right._value
+        else:
+            rightVal = 'null'
 
         print("value: {0}, left: {1}, right: {2}".
         format(val, leftVal, rightVal))
 
 class Tree(object):
     def __init__(self):
+        self._level = 0
+        self._root = None # TreeNode
         pass
 
     def create(self, numbers):
+        # Create a binary tree from a string of numbers
         # type numbers: a string array of numbers
         # rtype: TreeNode
         if not numbers:
@@ -100,6 +116,9 @@ class Tree(object):
             level += 1
             count = 2 ** level
 
+        self._level = level
+        self._root = root
+
         print(len(tree))
         for i in range(len(tree)):
             if not tree[i]:
@@ -107,4 +126,65 @@ class Tree(object):
             else:
                 tree[i].print_node()
 
-        return root
+        return self._root
+
+    def print_tree_level_order(self):
+        # Print a binary tree in level order
+        # type root: TreeNode
+        # rtype: string
+        if not self._root:
+            return None
+
+        treeList = list()
+        treeQueue = deque()
+        treeQueue.append(self._root) # Add to the right side of the deque
+
+        while(treeQueue):
+            tNode = treeQueue.popleft() # Pop from the left side of the deque
+            if tNode and not tNode.is_leaf():
+                # Add tNode's left and right children to the deque
+                if tNode.left:
+                    # tNode.left is a TreeNode,
+                    # tNode.left(), is trying to call a function name tNode.left()
+                    treeQueue.append(tNode.left)
+                else:
+                    treeQueue.append(None)
+                if tNode.right:
+                    treeQueue.append(tNode.right)
+                else:
+                    treeQueue.append(None)
+
+            if tNode:
+                treeList.append(tNode.value)
+            else:
+                treeList.append('null')
+
+        # Remove the last 'null' if existing
+        listLen = len(treeList)
+        while(treeList[listLen - 1] == 'null'):
+            treeList.pop()
+            listLen = len(treeList)
+
+        print(treeList)
+
+    def is_symmetric(self):
+        if not self._root:
+            return True
+
+        if self._root.is_leaf():
+            return True
+        else:
+            return self.check_symmetric(self._root.left, self._root.right)
+
+    def check_symmetric(self, leftTree, rightTree):
+        if not leftTree and not rightTree:
+            return True
+        elif not leftTree or not rightTree:
+            return False
+
+        # Both left and right children exist
+        if leftTree.value != rightTree.value:
+            return False
+        else:
+            return (self.check_symmetric(leftTree.left, rightTree.right) and
+                    self.check_symmetric(leftTree.right, rightTree.left))
